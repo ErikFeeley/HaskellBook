5e504d7eb78de99bd5b70cdcbe00e8b30ad15c50
Erik <ehfeeley@gmail.com>
2018-05-28 00:37:02 -0400

    cruising along

---
+++ chapter7/matchingTuples1.hs
@@ -0,0 +1,10 @@
+module TupleFunctions where
+
+addEmUp2 :: Num a => (a, a) -> a
+addEmUp2 (x, y) = x + y
+
+addEmUp2Alt :: Num a => (a, a) -> a
+addEmUp2Alt tup = (fst tup) + (snd tup)
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _ , _) = x
